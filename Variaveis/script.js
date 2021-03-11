0a786284269bc44d4525306a8367ccfa7e3d2b9e
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-03-10 21:21:56 -0300

    Primeiro Commit

---
+++ Variaveis/script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Thiago';
+
+
+// Declarar uma variável com a sua idade
+var idade = 40;
+
+// Declarar uma variável com a sua comida  favorita e não atribuir valor
+var comida;
+
+// Atribuir valor a sua comida favorita
+comida = 'risoto';
+
+
+// Declarar 5 variáveis diferentes sem valores
+var teste1, 
+    teste2,
+    teste3, 
+    teste4, 
+    teste5;
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,29 @@
+// Declare uma variável contendo uma string
+var nome = 'Thiago Henrique';
+// console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2018';
+
+// Declare uma variável com a sua idade
+var idade = 40;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobreNome = 'Martins da Silva';
+var nomeCompleto = `${nome} ${sobreNome}`;
+console.log(nomeCompleto);
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+console.log(frase);
+var frase = 'It\'s time';
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+console.log(verificarTipoNome);
+
+
+
